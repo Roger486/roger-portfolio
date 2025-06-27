0b6ec29e3df10ca4571192e0a8f5bf790d616e37
Roger <belegund@gmail.com>
2025-06-27 17:01:52 +0200

    Project init with vite (React19 + Tailwind v4)

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl bg-red-700 font-bold underline">
+      Roger's portfolio in React 19 with Tailwind CSS v4
+    </h1>
+  )
+}
+
+export default App
