6d9fa783df70b6b63b3ec21db0f2ef859a3dc31f
Roger <belegund@gmail.com>
2025-07-01 18:19:14 +0200

    feat: add first implementation of the skills section and the SkillCard.jsx component

--- src/features/skills/Skills.jsx
+++ src/features/skills/Skills.jsx
@@ -1,7 +1,22 @@
+import { FaReact } from "react-icons/fa";
+import SkillCard from "./SkillCard";
+
+
 export default function Skills() {
+  const skill = {
+    name: "React - JavaScript",
+    description: "Esta página web (mi portfolio) web está hecha por mí en React 19 con Tailwind v4.",
+    icon: FaReact,
+    color: "#00d8FF",
+    backgroundColor: "#222222"
+  };
+
   return (
-    <section id="skills" className="min-h-screen p-8 bg-gray-300/50">
+    <section id="skills" className="min-h-screen p-8">
       <h2 className="text-3xl font-bold mb-4">Skills</h2>
+      <SkillCard skill={skill} />
+
+      <br />
       <ul className="list-disc list-inside">
         <li>HTML5, CSS3, JavaScript</li>
         <li>React, React Hooks, Vite</li>
