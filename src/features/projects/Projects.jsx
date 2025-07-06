30c26b05d6ba0058da77bcbc48bd2c9daab09065
Roger <belegund@gmail.com>
2025-07-06 14:42:27 +0200

    chore: meaningless change to test auto deploy

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -1,7 +1,7 @@
 export default function Projects() {
   return (
     <section id="projects" className="min-h-screen p-8">
-      <h2 className="text-3xl font-bold mb-4">Projects</h2>
+      <h2 className="text-3xl font-bold mb-4">Proyectos</h2>
       <p>Aquí puedes mostrar tus proyectos con links, capturas o descripciones.</p>
       {/* Luego puedes añadir tarjetas o listas con tus proyectos */}
     </section>
