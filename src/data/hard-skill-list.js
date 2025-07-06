d28ac1cbc7c5726e5120e9cd85b6334be2944655
Roger <belegund@gmail.com>
2025-07-06 18:27:12 +0200

    content: add JavaFX, JavaScript, PHP, HTML/CSS, Tailwind to hard-skill-list.js

--- src/data/hard-skill-list.js
+++ src/data/hard-skill-list.js
@@ -1,36 +1,72 @@
+import { BiCodeAlt, BiWindowAlt } from "react-icons/bi";
 import { DiScrum } from "react-icons/di";
 import { FaAngular, FaDocker, FaGithub, FaJava, FaLaravel, FaRaspberryPi, FaReact } from "react-icons/fa";
 import { RiNodejsFill } from "react-icons/ri";
-import { SiMongodb, SiMysql } from "react-icons/si";
+import { SiHtml5, SiJavascript, SiMongodb, SiMysql, SiPhp, SiTailwindcss } from "react-icons/si";
 
 export const hardSkills = [
   {
-    key: "laravel-php",
-    name: "Laravel - PHP",
+    key: "laravel",
+    name: "Laravel",
     icon: FaLaravel,
     color: "white",
     backgroundColor: "#f55247",
   },
   {
-    key: "react-js",
-    name: "React - JavaScript",
+    key: "react",
+    name: "React",
     icon: FaReact,
     color: "#00d8FF",
     backgroundColor: "#222222",
   },
   {
-    key: "angular-js",
-    name: "Angular - JavaScript",
+    key: "angular",
+    name: "Angular",
     icon: FaAngular,
     color: "#c3002f",
     backgroundColor: "#eaeaea",
   },
   {
-    key: "java-javafx",
-    name: "Java - JavaFX",
+    key: "tailwind",
+    name: "Tailwind CSS",
+    icon: SiTailwindcss,
+    color: "#FFFFFF",
+    backgroundColor: "#38BDF8"
+  },
+  {
+    key: "htmlcss",
+    name: "HTML/CSS",
+    icon: SiHtml5,
+    color: "#FFFFFF",
+    backgroundColor: "#E34F26",
+  },
+  {
+    key: "javascript",
+    name: "JavaScript",
+    icon: SiJavascript,
+    color: "#000000",
+    backgroundColor: "#F7DF1E",
+  },
+  {
+    key: "php",
+    name: "PHP",
+    icon: SiPhp,
+    color: "#FFFFFF",
+    backgroundColor: "#777BB4",
+  },
+  {
+    key: "java",
+    name: "Java",
     icon: FaJava,
-    color: "#5382a1",
-    backgroundColor: "white",
+    color: "#007396",
+    backgroundColor: "#E0F0FF",
+  },
+  {
+    key: "javafx",
+    name: "JavaFX",
+    icon: BiWindowAlt,
+    color: "#007396",
+    backgroundColor: "#E0F0FF",
   },
   {
     key: "nodejs - JavaScript",
