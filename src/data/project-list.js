047cf2cb9a5f60238cf246d1ad8d97a0b0e5d84f
Roger <belegund@gmail.com>
2025-07-07 13:11:12 +0200

    content: add project-list.js with some basic projects to start the project component implementation

---
+++ src/data/project-list.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    key: 'hostcontrol',
+    roles: ['Backend Developer', 'Database Design', 'Technical & API Documentation'],
+    mainSkillsKeys : ['laravel', 'php', 'api-rest', 'mvc'],
+    secondarySkillsKeys: ['raspi-web-server', 'git', 'github', 'docker',],
+    repoUrls: [
+      {url: 'https://github.com/Roger486/HostControl', label: 'Main Repo'}
+    ],
+    demoUrls: [
+      { url: 'https://hostcontrol-vicentedev.rogerdev.xyz/', label: 'Frontend' },
+      { url: 'https://hostcontrol-api.rogerdev.xyz/api/', label: 'API' }
+    ],
+    images: [
+      { key: 'home', src: '/assets/projects/hostcontrol/home.png' }
+    ],
+    featured: true
+  },
+  {
+    key: 'raspi-server',
+    roles: ['Deployment Automation', 'Infrastructure Scripting', 'Technical Documentation'],
+    mainSkillsKeys : ['raspi-web-server', 'docker', 'bash'],
+    secondarySkillsKeys: ['git', 'github'],
+    repoUrls: [
+      {url: 'https://github.com/Roger486/raspi-server', label: 'Main Repo'}
+
+    ],
+    demoUrls: [],
+    images: [
+      { key: 'home', src: '/assets/projects/raspi-server/home.png' }
+    ],
+    featured: false
+  },
+  {
+    key: 'coffee-haven',
+    roles: ['Frontend Developer'],
+    mainSkillsKeys : ['angular', 'typescript', 'htmlcss'],
+    secondarySkillsKeys: ['docker', 'raspi-web-server', 'git', 'github'],
+    repoUrls: [
+      {url: 'https://github.com/Roger486/CoffeeHaven-Angular', label: 'Main Repo'}
+
+    ],
+    demoUrls: [
+      { url: 'https://coffee-haven.rogerdev.xyz/', label: 'Frontend' }
+    ],
+    images: [
+      { key: 'home', src: '/assets/projects/coffee-haven/home.png' }
+    ],
+    featured: false
+  },
+  {
+    key: 'roger-portfolio',
+    roles: ['Frontend Developer'],
+    mainSkillsKeys : ['react', 'javascript', 'tailwind', 'htmlcss'],
+    secondarySkillsKeys: ['docker', 'raspi-web-server', 'git', 'github'],
+    repoUrls: [
+      {url: 'https://github.com/Roger486/roger-portfolio', label: 'Main Repo'}
+    ],
+    demoUrls: [
+      { url: 'https://roger-portfolio.rogerdev.xyz/', label: 'Frontend' }
+    ],
+    images: [
+      { key: 'home', src: '/assets/projects/roger-portfolio/home.png' }
+    ],
+    featured: false
+  },
+];
