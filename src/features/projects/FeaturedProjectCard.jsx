86c37c5cb3f313482ffee62d41a2b04570e1ea2b
Roger <belegund@gmail.com>
2025-07-07 17:49:49 +0200

    refactor: add FeaturedProjectCard.jsx and moves all related code from Projects.jsx

---
+++ src/features/projects/FeaturedProjectCard.jsx
@@ -0,0 +1,124 @@
+import { hardSkills } from "../../data/hard-skill-list";
+
+export default function FeaturedProjectCard({ project }) {
+  return (
+    <div
+      className="
+        grid grid-cols-1 md:grid-cols-8 gap-4 p-4
+        border-2 rounded-xl bg-white border-blue-400 shadow-md
+        "
+    >
+      {/* Thumbnail + CTA */}
+      <div className="col-span-3">
+        <div className="mb-2">
+          <img
+            src={project.thumbnail}
+            alt="Thumbnail"
+            className="
+                rounded border-1 border-black/20 shadow
+                hover:scale-105 transition duration-300
+              "
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <h4>Demo URLs:</h4>
+          <div className="flex gap-1 justify-center text-center">
+            {project.demoUrls.map((demo) => {
+              return (
+                <a
+                  key={demo.url}
+                  href={demo.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="
+                      text-sm rounded px-2 min-w-15 text-white bg-blue-600
+                      hover:bg-blue-400
+                    "
+                >
+                  {demo.label}
+                </a>
+              );
+            })}
+          </div>
+
+          <h4>Repo URLs:</h4>
+          <div className="flex gap-1 justify-center text-center">
+            {project.repoUrls.map((repo) => {
+              return (
+                <a
+                  key={repo.url}
+                  href={repo.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="
+                      text-sm border rounded px-2 min-w-15 border-black/20
+                      hover:bg-gray-200
+                    "
+                >
+                  {repo.label}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+      {/* Title + text + roles + skills */}
+      <div className="col-span-5">
+        <h3 className="text-2xl font-bold mb-4">{project.name}</h3>
+        <h4 className="font-semibold mb-2">Project description:</h4>
+        <p className="text-sm mb-2">
+          Project description. To be implemented on the es.json and en.son
+          files.
+        </p>
+        <h4>Roles:</h4>
+        <div className="flex flex-wrap gap-2">
+          {project.roles.map((role) => {
+            return (
+              <span key={role} className="text-sm bg-orange-300 rounded px-2">
+                {role}
+              </span>
+            );
+          })}
+        </div>
+        <div>
+          <h4>Main Skills:</h4>
+          <div className="flex flex-wrap gap-2">
+            {project.mainSkillsKeys.map((skillKey) => {
+              const foundSkill = hardSkills.find(
+                (hardSkill) => hardSkill.key === skillKey
+              );
+              const skillName = foundSkill ? foundSkill.name : skillKey;
+              return (
+                <span
+                  key={skillKey}
+                  className="text-sm bg-orange-300 rounded px-2"
+                >
+                  {skillName}
+                </span>
+              );
+            })}
+          </div>
+          <h4>Secondary Skills:</h4>
+          <div className="flex flex-wrap gap-2">
+            {project.secondarySkillsKeys.map((skillKey) => {
+              const foundSkill = hardSkills.find(
+                (hardSkill) => hardSkill.key === skillKey
+              );
+              const skillName = foundSkill ? foundSkill.name : skillKey;
+              return (
+                <span
+                  key={skillKey}
+                  className="text-sm bg-orange-300 rounded px-2"
+                >
+                  {skillName}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
