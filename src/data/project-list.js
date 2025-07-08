b1f87de36d1a86445cde6d1b95d09bf8815cbc04
Roger <belegund@gmail.com>
2025-07-08 11:23:22 +0200

    content: improved project-list.js with more info

--- src/data/project-list.js
+++ src/data/project-list.js
@@ -6,12 +6,12 @@ export const projects = [
     mainSkillsKeys : ['laravel', 'php', 'api-rest', 'mvc'],
     secondarySkillsKeys: ['raspi-web-server', 'git', 'github', 'docker', 'agile'],
     repoUrls: [
-      {url: 'https://github.com/Roger486/HostControl/tree/main/backend', label: 'API Repo'},
-      {url: 'https://github.com/Roger486/HostControl', label: 'Main Repo'}
+      {url: 'https://github.com/Roger486/HostControl/tree/main/backend', label: 'API Repo', destination: 'github' },
+      {url: 'https://github.com/Roger486/HostControl', label: 'Main Repo', destination: 'github' }
     ],
     demoUrls: [
-      { url: 'https://hostcontrol-api.rogerdev.xyz/api/accommodations', label: 'API' },
-      { url: 'https://hostcontrol-vicentedev.rogerdev.xyz/', label: 'Frontend' }
+      { url: 'https://hostcontrol-api.rogerdev.xyz/api/accommodations', label: 'API', destination: 'default' },
+      { url: 'https://hostcontrol-vicentedev.rogerdev.xyz/', label: 'Frontend', destination: 'default' }
     ],
     thumbnail: '/projects/hostcontrol/hostcontrol-thumb.jpg',
     images: [
@@ -30,12 +30,12 @@ export const projects = [
     mainSkillsKeys : ['raspi-web-server', 'docker', 'bash'],
     secondarySkillsKeys: ['git', 'github'],
     repoUrls: [
-      {url: 'https://github.com/Roger486/raspi-server', label: 'Main Repo'}
+      {url: 'https://github.com/Roger486/raspi-server', label: 'Main Repo', destination: 'github' }
 
     ],
     demoUrls: [],
     images: [
-      { key: 'home', src: '/assets/projects/raspi-server/home.png' }
+      { key: 'home', src: '/assets/projects/raspi-server/home.png', destination: 'default' }
     ],
     featured: false
   },
@@ -46,11 +46,11 @@ export const projects = [
     mainSkillsKeys : ['angular', 'typescript', 'htmlcss'],
     secondarySkillsKeys: ['docker', 'raspi-web-server', 'git', 'github'],
     repoUrls: [
-      {url: 'https://github.com/Roger486/CoffeeHaven-Angular', label: 'Main Repo'}
+      {url: 'https://github.com/Roger486/CoffeeHaven-Angular', label: 'Main Repo', destination: 'github' }
 
     ],
     demoUrls: [
-      { url: 'https://coffee-haven.rogerdev.xyz/', label: 'Frontend' }
+      { url: 'https://coffee-haven.rogerdev.xyz/', label: 'Frontend', destination: 'default' }
     ],
     images: [
       { key: 'home', src: '/assets/projects/coffee-haven/home.png' }
@@ -64,10 +64,10 @@ export const projects = [
     mainSkillsKeys : ['react', 'javascript', 'tailwind', 'htmlcss'],
     secondarySkillsKeys: ['docker', 'raspi-web-server', 'git', 'github'],
     repoUrls: [
-      {url: 'https://github.com/Roger486/roger-portfolio', label: 'Main Repo'}
+      {url: 'https://github.com/Roger486/roger-portfolio', label: 'Main Repo', destination: 'github' }
     ],
     demoUrls: [
-      { url: 'https://roger-portfolio.rogerdev.xyz/', label: 'Frontend' }
+      { url: 'https://roger-portfolio.rogerdev.xyz/', label: 'Frontend', destination: 'default' }
     ],
     images: [
       { key: 'home', src: '/assets/projects/roger-portfolio/home.png' }
