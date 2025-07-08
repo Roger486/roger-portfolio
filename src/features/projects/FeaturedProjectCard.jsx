7107e4eb6ba3d29c37064fa475297574ee01e139
Roger <belegund@gmail.com>
2025-07-08 10:59:00 +0200

    refactor(FeaturedProjectCard): moved out logic from the return section of the component

--- src/features/projects/FeaturedProjectCard.jsx
+++ src/features/projects/FeaturedProjectCard.jsx
@@ -2,6 +2,93 @@ import { FaGithub, FaLink } from "react-icons/fa";
 import { hardSkills } from "../../data/hard-skill-list";
 
 export default function FeaturedProjectCard({ project }) {
+  const demoUrlElements = project.demoUrls.map((demo) => {
+    return (
+      <a
+        key={demo.url}
+        href={demo.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="
+          flex gap-1 items-center
+          text-sm rounded px-3 py-1 min-w-15 text-white bg-blue-700
+          transition duration-300
+          hover:bg-blue-400
+        "
+      >
+        <FaLink /> {demo.label}
+      </a>
+    );
+  });
+
+  const repoUrlElements = project.repoUrls.map((repo) => {
+    return (
+      <a
+        key={repo.url}
+        href={repo.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="
+          flex gap-1 items-center
+          text-sm border rounded px-3 py-1 min-w-15 border-black/20
+          transition duration-300
+          hover:bg-blue-400 hover:text-white
+        "
+      >
+        <FaGithub size={"1.1rem"} /> {repo.label}
+      </a>
+    );
+  });
+
+  const roleElements = project.roles.map((role) => {
+    return (
+      <span
+        key={role}
+        className="text-sm text-white border  bg-blue-400 rounded px-2"
+      >
+        {role}
+      </span>
+    );
+  });
+
+  const mainSkillElements = project.mainSkillsKeys.map((skillKey) => {
+    const foundSkill = hardSkills.find(
+      (hardSkill) => hardSkill.key === skillKey
+    );
+    const skillName = foundSkill ? foundSkill.name : skillKey;
+    const skillBackgroundColor = foundSkill
+      ? foundSkill.backgroundColor
+      : "#000000";
+    const skillColor = foundSkill ? foundSkill.color : "#FFFFFF";
+    return (
+      <span
+        key={skillKey}
+        className="text-sm rounded px-2 border"
+        style={{
+          color: skillColor,
+          backgroundColor: skillBackgroundColor,
+        }}
+      >
+        {skillName}
+      </span>
+    );
+  });
+
+  const secondarySkillElements = project.secondarySkillsKeys.map((skillKey) => {
+    const foundSkill = hardSkills.find(
+      (hardSkill) => hardSkill.key === skillKey
+    );
+    const skillName = foundSkill ? foundSkill.name : skillKey;
+    return (
+      <span
+        key={skillKey}
+        className="text-sm bg-gray-100 border border-black/50 rounded px-2"
+      >
+        {skillName}
+      </span>
+    );
+  });
+
   return (
     <div
       className="
@@ -25,46 +112,12 @@ export default function FeaturedProjectCard({ project }) {
         <div>
           <h4>Demo URLs:</h4>
           <div className="mb-4 flex gap-1 justify-center text-center">
-            {project.demoUrls.map((demo) => {
-              return (
-                <a
-                  key={demo.url}
-                  href={demo.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="
-                  flex gap-1 items-center
-                      text-sm rounded px-3 py-1 min-w-15 text-white bg-blue-700
-                      transition duration-300
-                      hover:bg-blue-400
-                    "
-                >
-                  <FaLink />{demo.label}
-                </a>
-              );
-            })}
+            {demoUrlElements}
           </div>
 
           <h4>Repo URLs:</h4>
           <div className="flex gap-1 justify-center text-center">
-            {project.repoUrls.map((repo) => {
-              return (
-                <a
-                  key={repo.url}
-                  href={repo.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="
-                      flex gap-1 items-center
-                      text-sm border rounded px-3 py-1 min-w-15 border-black/20
-                      transition duration-300
-                      hover:bg-blue-400 hover:text-white
-                    "
-                >
-                  <FaGithub size={"1.1rem"} />{repo.label}
-                </a>
-              );
-            })}
+            {repoUrlElements}
           </div>
         </div>
       </div>
@@ -74,57 +127,16 @@ export default function FeaturedProjectCard({ project }) {
         <h3 className="text-2xl font-bold mb-4">{project.name}</h3>
         <h4 className="font-semibold mb-2">Project description:</h4>
         <p className="text-sm mb-2">
-          Project description. To be implemented on the es.json and en.son
+          Project description. To be implemented on the es.json and en.json
           files.
         </p>
         <h4 className="mt-4 font-semibold">Roles:</h4>
-        <div className="flex flex-wrap gap-1">
-          {project.roles.map((role) => {
-            return (
-              <span key={role} className="text-sm text-white border  bg-blue-400 rounded px-2">
-                {role}
-              </span>
-            );
-          })}
-        </div>
+        <div className="flex flex-wrap gap-1">{roleElements}</div>
         <div>
           <h4 className="mt-4 font-semibold">Main Skills:</h4>
-          <div className="flex flex-wrap gap-2">
-            {project.mainSkillsKeys.map((skillKey) => {
-              const foundSkill = hardSkills.find(
-                (hardSkill) => hardSkill.key === skillKey
-              );
-              const skillName = foundSkill ? foundSkill.name : skillKey;
-              const skillBackgroundColor = foundSkill ? foundSkill.backgroundColor : "#000000";
-              const skillColor = foundSkill ? foundSkill.color : "#FFFFFF";
-              return (
-                <span
-                  key={skillKey}
-                  className="text-sm rounded px-2 border"
-                  style={{color:skillColor, backgroundColor:skillBackgroundColor}}
-                >
-                  {skillName}
-                </span>
-              );
-            })}
-          </div>
+          <div className="flex flex-wrap gap-2">{mainSkillElements}</div>
           <h4 className="mt-4 font-semibold">Secondary Skills:</h4>
-          <div className="flex flex-wrap gap-2">
-            {project.secondarySkillsKeys.map((skillKey) => {
-              const foundSkill = hardSkills.find(
-                (hardSkill) => hardSkill.key === skillKey
-              );
-              const skillName = foundSkill ? foundSkill.name : skillKey;
-              return (
-                <span
-                  key={skillKey}
-                  className="text-sm bg-gray-100 border border-black/50 rounded px-2"
-                >
-                  {skillName}
-                </span>
-              );
-            })}
-          </div>
+          <div className="flex flex-wrap gap-2">{secondarySkillElements}</div>
         </div>
       </div>
     </div>
