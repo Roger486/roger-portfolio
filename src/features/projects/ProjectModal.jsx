f6f72d8467ad3f97bfde2df31b7234be718f4edc
Roger <belegund@gmail.com>
2025-07-09 16:31:46 +0200

    feature: add use of the createPortal() functionality of React for the project modals

--- src/features/projects/ProjectModal.jsx
+++ src/features/projects/ProjectModal.jsx
@@ -2,11 +2,10 @@ import { ImCross } from "react-icons/im";
 import { useLanguage } from "../../context/LanguageContext";
 import { useEffect } from "react";
 import { lockBodyScroll, unlockBodyScroll } from "../../helpers/lockbodyscroll";
+import { createPortal } from "react-dom";
 
 export default function ProjectModal({ project, onClose }) {
   const { t } = useLanguage();
-
-
   const isOpen = Boolean(project);
 
   useEffect(() => {
@@ -14,14 +13,13 @@ export default function ProjectModal({ project, onClose }) {
     return () => unlockBodyScroll();
   }, [isOpen]);
 
-  // never put a return before a useEffect
   if (!isOpen) return null;
 
+  const modalRoot = document.getElementById("project-modal-root");
 
-  
-  return (
+  const modalContent = (
     <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg max-w-2xl w-full relative">
+      <div className="bg-white p-6 rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto relative">
         <button
           className="absolute top-4 right-4 text-gray-400 hover:text-red-400"
           onClick={onClose}
@@ -39,4 +37,6 @@ export default function ProjectModal({ project, onClose }) {
       </div>
     </div>
   );
+
+  return createPortal(modalContent, modalRoot);
 }
