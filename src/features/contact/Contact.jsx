a902d2c3d7caaf5f664de2e8b471decb72be0351
Roger <belegund@gmail.com>
2025-07-11 18:19:07 +0200

    chore: minor detail

--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <section id="contact" className="min-h-screen p-8">
       <h2 className="text-3xl font-bold mb-4">Contact</h2>
-      <p>Incluye un formulario de contacto o tus datos de contacto aquí.</p>
+      <p>WIP...</p>
     </section>
   );
 }
