279b90b0e3fd343083b6be48839831f77a485ac4
Roger <belegund@gmail.com>
2025-07-16 19:40:19 +0200

    style: add animations to all sections

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -5,10 +5,13 @@ import FeaturedProjectCard from "./components/FeaturedProjectCard";
 import ProjectCard from "./components/ProjectCard";
 import ProjectModal from "./ProjectModal";
 import SectionTitle from "../../components/ui/SectionTitle";
+import useInView from "../../hooks/useInView";
 
 export default function Projects() {
   const { t } = useLanguage();
   const [selectedProject, setSelectedProject] = useState(null);
+  const { ref: featuredProjectsGridRef, hasBeenVisible: featuredProjectssGridHasBeenVisible } = useInView(0.1);
+  const { ref: projectsGridRef, hasBeenVisible: projectssGridHasBeenVisible } = useInView(0.1);
 
   const handleOpenProjectModal = (project) => {
     setSelectedProject(project);
@@ -41,8 +44,22 @@ export default function Projects() {
   return (
     <section id="projects" className="min-h-screen p-8">
       <SectionTitle>{t("projects.title")}</SectionTitle>
-      <div className="flex flex-col gap-4 mb-4">{featuredProjectElements}</div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div 
+        ref={featuredProjectsGridRef}
+        className={`
+          flex flex-col gap-4 mb-4 opacity-0
+          ${featuredProjectssGridHasBeenVisible ? "fade-in" : ""}
+        `}
+      >
+        {featuredProjectElements}
+        </div>
+      <div
+        ref={projectsGridRef}
+        className={`
+          grid grid-cols-1 md:grid-cols-2 gap-4 opacity-0
+          ${projectssGridHasBeenVisible ? "fade-in" : ""}
+        `}
+      >
         {normalProjectElements}
       </div>
 
--- src/features/skills/Skills.jsx
+++ src/features/skills/Skills.jsx
@@ -4,9 +4,14 @@ import { softSkills } from "../../data/soft-skill-list";
 import { useLanguage } from "../../context/LanguageContext";
 import SoftSkillCard from "./SoftSkillCard";
 import SectionTitle from "../../components/ui/SectionTitle";
+import useInView from "../../hooks/useInView";
 
 export default function Skills() {
   const { t } = useLanguage();
+  const { ref: softSkillsTitleRef, isVisible: softSkillsTitleIsVisible } = useInView();
+  const { ref: hardSkillsTitleRef, isVisible: hardSkillsTitleIsVisible } = useInView();
+  const { ref: softSkillsGridRef, hasBeenVisible: softSkillsGridHasBeenVisible } = useInView(0.1);
+  const { ref: hardSkillsGridRef, hasBeenVisible: hardSkillsGridHasBeenVisible } = useInView(0.1);
   const hardSkillElements = getHardSkills();
   const softSkillElements = getSoftSkills();
 
@@ -34,12 +39,40 @@ export default function Skills() {
   return (
     <section id="skills" className="min-h-screen p-4">
       <SectionTitle>{t("skills.title")}</SectionTitle>
-      <h3 className="text-xl font-bold mb-4">{t("skills.title-hard")}</h3>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-8">
+      <h3
+        ref={softSkillsTitleRef}
+        className={`
+          text-xl font-bold mb-4 text-blue-600
+          ${softSkillsTitleIsVisible ? "fade-in-lateral" : ""}
+        `}
+      >
+        {t("skills.title-hard")}
+      </h3>
+      <div
+        ref={softSkillsGridRef}
+        className={`
+          grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-8 opacity-0
+          ${softSkillsGridHasBeenVisible ? "fade-in" : ""}
+        `}
+      >
         {hardSkillElements}
       </div>
-      <h3 className="text-xl font-bold mb-4">{t("skills.title-soft")}</h3>
-      <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-4">
+      <h3
+        ref={hardSkillsTitleRef}
+        className={`
+          text-xl font-bold mb-4 text-blue-600
+          ${hardSkillsTitleIsVisible ? "fade-in-lateral" : ""}
+        `}
+      >
+        {t("skills.title-soft")}
+      </h3>
+      <div
+        ref={hardSkillsGridRef}
+        className={`
+          grid md:grid-cols-1 lg:grid-cols-2 gap-4 opacity-0
+          ${hardSkillsGridHasBeenVisible ? "fade-in" : ""}
+        `}
+      >
         {softSkillElements}
       </div>
     </section>
