f7cab7a50e6d8788a0a318236edb1925ec27faae
Roger <belegund@gmail.com>
2025-07-20 13:55:22 +0200

    fix: fix fade-in-lateral animation not working properly on small device screens

--- src/features/skills/pages/Skills.jsx
+++ src/features/skills/pages/Skills.jsx
@@ -5,6 +5,7 @@ import { useLanguage } from "../../../core/context/LanguageContext";
 import SoftSkillCard from "../components/SoftSkillCard";
 import SectionTitle from "../../../core/components/ui/SectionTitle";
 import useInView from "../../../core/hooks/useInView";
+import HardSkillCategoryCard from "../components/HardSkillCategoryCard";
 
 export default function Skills() {
   const { t } = useLanguage();
@@ -12,13 +13,17 @@ export default function Skills() {
   const { ref: hardSkillsTitleRef, isVisible: hardSkillsTitleIsVisible } = useInView();
   const { ref: softSkillsGridRef, hasBeenVisible: softSkillsGridHasBeenVisible } = useInView(0.1);
   const { ref: hardSkillsGridRef, hasBeenVisible: hardSkillsGridHasBeenVisible } = useInView(0.1);
-  const hardSkillElements = getHardSkills();
+  const hardSkillElements = getHardSkillCategoryCards();
   const softSkillElements = getSoftSkills();
 
   function getHardSkills() {
     return hardSkills.map(category => category.skills.map((skill) => {
       return <HardSkillCard key={skill.key} skill={skill} />;
     })).flat();
+  }
+
+  function getHardSkillCategoryCards() {
+    return hardSkills.map(category => <HardSkillCategoryCard key={category.categoryKey} category={category} />)
   }
 
   function getSoftSkills() {
@@ -40,37 +45,37 @@ export default function Skills() {
     <section id="skills" className="min-h-screen p-4">
       <SectionTitle>{t("skills.title")}</SectionTitle>
       <h3
-        ref={softSkillsTitleRef}
-        className={`
-          text-xl font-bold mb-4 text-blue-600
-          ${softSkillsTitleIsVisible ? "fade-in-lateral" : ""}
-        `}
-      >
-        {t("skills.title-hard")}
-      </h3>
-      <div
-        ref={softSkillsGridRef}
-        className={`
-          grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-8 opacity-0
-          ${softSkillsGridHasBeenVisible ? "fade-in" : ""}
-        `}
-      >
-        {hardSkillElements}
-      </div>
-      <h3
         ref={hardSkillsTitleRef}
         className={`
           text-xl font-bold mb-4 text-blue-600
           ${hardSkillsTitleIsVisible ? "fade-in-lateral" : ""}
         `}
       >
-        {t("skills.title-soft")}
+        {t("skills.title-hard")}
       </h3>
       <div
         ref={hardSkillsGridRef}
         className={`
-          grid md:grid-cols-1 lg:grid-cols-2 gap-4 opacity-0
+          grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-8 opacity-0
           ${hardSkillsGridHasBeenVisible ? "fade-in" : ""}
+        `}
+      >
+        {hardSkillElements}
+      </div>
+      <h3
+        ref={softSkillsTitleRef}
+        className={`
+          text-xl font-bold mb-4 text-blue-600
+          ${softSkillsTitleIsVisible ? "fade-in-lateral" : ""}
+        `}
+      >
+        {t("skills.title-soft")}
+      </h3>
+      <div
+        ref={softSkillsGridRef}
+        className={`
+          grid md:grid-cols-1 lg:grid-cols-2 gap-4 opacity-0
+          ${softSkillsGridHasBeenVisible ? "fade-in" : ""}
         `}
       >
         {softSkillElements}
