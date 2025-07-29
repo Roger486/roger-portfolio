a077d895a1d8080f1d0395a0d10bc509c24f83db
Roger <belegund@gmail.com>
2025-07-29 11:45:35 +0200

    fix: fix contact me floating button moving because of lateral animations

--- src/core/layouts/MainLayout.jsx
+++ src/core/layouts/MainLayout.jsx
@@ -35,7 +35,8 @@ export default function MainLayout({ children }) {
       <Topbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <Sidebar isMenuOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} activeSection={activeSection} />
 
-      <main className="ml-0 lg:ml-64 px-4 sm:px-6 md:px-8 lg:px-0 lg:pr-8">
+      {/* overflow-x-hidden used to prevent lateral animations from modifying the vh width */}
+      <main className="ml-0 lg:ml-64 px-4 sm:px-6 md:px-8 lg:px-0 lg:pr-8 overflow-x-hidden">
         {children}
       </main>
 
