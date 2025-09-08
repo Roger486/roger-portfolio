ffb732ea56ed3a72d174f5ac7e2a8ecf708f71cd
Roger <belegund@gmail.com>
2025-09-08 16:26:14 +0200

    style: change double button of the languageSwitcher for a topdown list on click to better suit more languages i nthe future

--- src/core/components/ui/LanguageSwitcher.jsx
+++ src/core/components/ui/LanguageSwitcher.jsx
@@ -1,22 +1,63 @@
+import { useEffect, useRef, useState } from "react";
 import { useLanguage } from "../../context/LanguageContext";
+import { langs } from "../../data/langs";
 
 export default function LanguageSwitcher() {
   const { language, setLanguage } = useLanguage();
+  const [isLangListOpen, setIsLangListOpen] = useState(false);
+  const componentRef = useRef(null);
+
+  const onLangChoice = (code) => {
+    setLanguage(code);
+    setIsLangListOpen(false);
+  };
+
+  useEffect(() => {
+    if (!isLangListOpen) return;
+
+    const handleClickAnywhere = (e) => {
+      if (componentRef.current.contains(e.target)) return;
+      setIsLangListOpen(false);
+    }
+
+    const handleEscKey = (e) => {
+      if (e.key === 'Escape') setIsLangListOpen(false);
+    }
+
+    document.addEventListener('keydown', handleEscKey);
+    document.addEventListener('click', handleClickAnywhere);
+    return () => {
+      document.removeEventListener('keydown', handleEscKey);
+      document.removeEventListener('click', handleClickAnywhere);
+    }
+  }, [isLangListOpen])
+
+  const langDetails = langs.find((lang) => lang.code === language);
 
   return (
-    <div className="flex gap-2">
+    <div ref={componentRef}>
       <button
-        onClick={() => setLanguage("es")}
-        className={`px-2 py-1 text-xs rounded ${language === "es" ? "bg-gray-600 text-white" : ""}`}
+        type="button"
+        onClick={() => setIsLangListOpen(true)}
+        className={`w-24 p-2 text-xs rounded flex items-center gap-2 text-gray-500 bg-white border border-gray-500 ${isLangListOpen ? "opacity-30" : ""}`}
       >
-        Español
+        <img src={langDetails.flag} alt={langDetails.label} className="h-4 w-5" />
+        {langDetails.label}
       </button>
-      <button
-        onClick={() => setLanguage("en")}
-        className={`px-2 py-1 text-xs rounded ${language === "en" ? "bg-gray-600 text-white" : ""}`}
-      >
-        English
-      </button>
+      {isLangListOpen && (
+        <ul className="relative -mt-10 flex flex-col bg-gray-500 text-white text-xs rounded">
+          {langs.map((lang) => (
+            <li key={lang.code}><button
+              onClick={() => onLangChoice(lang.code)}
+              type="button"
+              className="w-24 p-2 flex items-center gap-2"
+            >
+              <img src={lang.flag} alt={lang.label} className="h-4 w-5" />
+              {lang.label}
+            </button></li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
---
+++ src/core/data/langs.js
@@ -0,0 +1,5 @@
+export const langs = [
+  {code: 'en', label: 'English', flag: '/flags/gb.svg'},
+  {code: 'es', label: 'Español', flag: '/flags/es.svg'},
+  //{code: 'ca', label: 'Català', flag: '/flags/ca.svg'}
+]
